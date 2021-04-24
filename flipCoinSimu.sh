fec7cf2fb72387d649327f45c85f07759ce7e7df
shivam satpute <shivam897satpute@gmail.com>
2021-04-24 16:07:25 +0530

    added: numberOfTimesHeadOrTailWin

--- flipCoinSimu.sh
+++ flipCoinSimu.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
-checkRandom=$(($RANDOM%2))
+headcount=0
+tailcount=0
 
-if [[ $checkRandom -eq 0 ]]
-then
-	echo "TAILS"
-else
-	echo "HEADS"
-fi
+
+for (( i=0 ; i<10 ; i++ ))
+do
+	checkRandom=$(($RANDOM%2))
+	if [[ $checkRandom -eq 0 ]]
+	then
+		tailcount=$(($tailcount+1))
+	elif [[ $checkRandom -ne 0 ]]
+	then
+		headcount=$(($headcount+1))
+	fi
+done
+
+echo "Number of Tail wins:" $tailcount
+echo "Number of Head Wins:" $headcount
