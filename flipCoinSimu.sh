ecf8dd15027f5d6bb4b2fc48fe96d1d3bc01c32d
shivam satpute <shivam897satpute@gmail.com>
2021-04-24 16:11:56 +0530

    added: Either have won 21 times. show win or tie

--- flipCoinSimu.sh
+++ flipCoinSimu.sh
@@ -2,19 +2,31 @@
 
 headcount=0
 tailcount=0
+count=1
 
-
-for (( i=0 ; i<10 ; i++ ))
+while [ $headcount -le 21 ] || [ $tailcount -le 21 ]
 do
-	checkRandom=$(($RANDOM%2))
-	if [[ $checkRandom -eq 0 ]]
+	result=$(($RANDOM%2))
+	if [[ $result -eq 0 ]]
 	then
-		tailcount=$(($tailcount+1))
-	elif [[ $checkRandom -ne 0 ]]
-	then
+		echo "HEADS"
 		headcount=$(($headcount+1))
+	elif [[ $result -eq 1 ]]
+	then
+		echo "TAILS"
+		tailcount=$(($tailcount+1))
 	fi
-done
 
-echo "Number of Tail wins:" $tailcount
-echo "Number of Head Wins:" $headcount
+	if [ $headcount -eq 21 ]
+	then
+		echo "Head wins 21 times!"
+		exit
+	elif [ $tailcount -eq 21 ]
+	then
+		echo "Tail wins 21 times!"
+		exit
+	else
+		echo "It's Tie!!"
+	fi
+	i=$(($i+1))
+done
